8c5ca1e9fcb1e6e38ac0961ad8935037b81b7fb7
syhw <gabriel.synnaeve@gmail.com>
2013-07-19 11:53:08 +0200

    smarter caching in prep_timit.py

--- DBN/prep_timit.py
+++ DBN/prep_timit.py
@@ -5,6 +5,8 @@ import numpy as np
 
 N_FRAMES = 11 # 5 before and 5 after the labeled frame
 BORROW = True
+USE_CACHING = True # beware if you use RBM / GRBM alternatively, set it to False
+TRAIN_CLASSIFIERS = False # train sklearn classifiers to compare the DBN to
 
 def padding(nframes, x, y):
     # dirty hacky padding
@@ -95,47 +97,65 @@ def prep_data(dataset, nframes=1, unit=False, normalize=False):
     for i, e in enumerate(test_y):
         test_y_f[i] = to_int[e]
 
-    ### Training a SVM to compare results TODO
-    #print "training a SVM" TODO
+    if TRAIN_CLASSIFIERS:
+        ### Training a SVM to compare results TODO
+        #print "training a SVM" TODO
 
-    ### Training a linear model (elasticnet) to compare results
-    print "training a linear model with SGD"
-    from sklearn import linear_model
-    from sklearn.cross_validation import cross_val_score
-    clf = linear_model.SGDClassifier(loss='modified_huber', penalty='elasticnet') # TODO change and CV params
-    clf.fit(train_x, train_y_f)
-    scores = cross_val_score(clf, test_x, test_y_f)
-    print "score linear classifier (elasticnet, SGD trained)", scores.mean()
-    with open('linear_elasticnet_classif.pickle', 'w') as f:
-        cPickle.dump(clf, f)
+        ### Training a linear model (elasticnet) to compare results
+        print "training a linear model with SGD"
+        from sklearn import linear_model
+        from sklearn.cross_validation import cross_val_score
+        clf = linear_model.SGDClassifier(loss='modified_huber', penalty='elasticnet') # TODO change and CV params
+        clf.fit(train_x, train_y_f)
+        scores = cross_val_score(clf, test_x, test_y_f)
+        print "score linear classifier (elasticnet, SGD trained)", scores.mean()
+        with open('linear_elasticnet_classif.pickle', 'w') as f:
+            cPickle.dump(clf, f)
 
-    ### Training a random forest to compare results
-    print "training a random forest"
-    from sklearn.ensemble import RandomForestClassifier
-    clf2 = RandomForestClassifier(n_jobs=-1, max_depth=None, min_samples_split=3) # TODO change and CV params
-    clf2.fit(train_x, train_y_f)
-    scores2 = cross_val_score(clf2, test_x, test_y_f)
-    print "score random forest", scores2.mean()
-    with open('random_forest_classif.pickle', 'w') as f:
-        cPickle.dump(clf2, f)
+        ### Training a random forest to compare results
+        print "training a random forest"
+        from sklearn.ensemble import RandomForestClassifier
+        clf2 = RandomForestClassifier(n_jobs=-1, max_depth=None, min_samples_split=3) # TODO change and CV params
+        clf2.fit(train_x, train_y_f)
+        scores2 = cross_val_score(clf2, test_x, test_y_f)
+        print "score random forest", scores2.mean()
+        with open('random_forest_classif.pickle', 'w') as f:
+            cPickle.dump(clf2, f)
 
-    with open('train_x.npy', 'w') as f:
-        np.save(f, train_x_f)
-    with open('train_y.npy', 'w') as f:
-        np.save(f, train_y_f)
-    with open('test_x.npy', 'w') as f:
-        np.save(f, test_x_f)
-    with open('test_y.npy', 'w') as f:
-        np.save(f, test_y_f)
-    print ">>> Serialized all train/test tables"
     return [train_x_f, train_y_f, test_x_f, test_y_f]
 
-def load_data(dataset, nframes=N_FRAMES, unit=False, normalize=False):
-    [train_x, train_y, test_x, test_y] = prep_data(dataset, 
-            nframes=nframes, unit=False, normalize=False)
+def load_data(dataset, nframes=N_FRAMES, unit=False, normalize=False, cv_frac=0.2):
+    def prep_and_serialize():
+        [train_x, train_y, test_x, test_y] = prep_data(dataset, 
+                nframes=nframes, unit=False, normalize=False)
+        with open('train_x_' + str(N_FRAMES) + '.npy', 'w') as f:
+            np.save(f, train_x)
+        with open('train_y_' + str(N_FRAMES) + '.npy', 'w') as f:
+            np.save(f, train_y)
+        with open('test_x_' + str(N_FRAMES) + '.npy', 'w') as f:
+            np.save(f, test_x)
+        with open('test_y_' + str(N_FRAMES) + '.npy', 'w') as f:
+            np.save(f, test_y)
+        print ">>> Serialized all train/test tables"
+        return [train_x, train_y, test_x, test_y]
+
+    if USE_CACHING:
+        try: # try to load from serialized filed, bewa
+            with open('train_x_' + str(N_FRAMES) + '.npy') as f:
+                train_x = np.load(f)
+            with open('train_y_' + str(N_FRAMES) + '.npy') as f:
+                train_y = np.load(f)
+            with open('test_x_' + str(N_FRAMES) + '.npy') as f:
+                test_x = np.load(f)
+            with open('test_y_' + str(N_FRAMES) + '.npy') as f:
+                test_y = np.load(f)
+        except: # do the whole preparation (normalization / padding)
+            [train_x, train_y, test_x, test_y] = prep_and_serialize()
+    else:
+        [train_x, train_y, test_x, test_y] = prep_and_serialize()
 
     from sklearn import cross_validation
-    X_train, X_validate, y_train, y_validate = cross_validation.train_test_split(train_x, train_y, test_size=0.15, random_state=0) # 15% for validation
+    X_train, X_validate, y_train, y_validate = cross_validation.train_test_split(train_x, train_y, test_size=cv_frac, random_state=0)
     
     train_set_x = theano.shared(X_train, borrow=BORROW)
     train_set_y = theano.shared(np.asarray(y_train, dtype=theano.config.floatX), borrow=BORROW)
