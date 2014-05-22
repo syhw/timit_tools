cfafed09177c287988bb92b1ac96094585db62e6
syhw <gabriel.synnaeve@gmail.com>
2014-05-22 16:52:11 +0200

    now refactored a little

--- DBN/DBN_spk_phn.py
+++ DBN/DBN_spk_phn.py
@@ -13,7 +13,8 @@ import theano.tensor as T
 from theano.tensor.shared_randomstreams import RandomStreams
 from theano import shared
 
-from logistic_timit import LogisticRegression 
+from logistic_regression import LogisticRegression 
+from dataset_sentences_iterator import DatasetSentencesIterator
 from mlp import HiddenLayer
 from relu_rbm import RBM
 from relu_grbm import GRBM
@@ -33,52 +34,6 @@ PRELEARNING_RATE_DENOMINATOR_FOR_GAUSSIAN = 50. # we take a lower learning rate
 MIN_FRAMES_PER_SENTENCE = 26
 BORROW = True
 output_file_name = 'dbn_spk_phn_13_frames'
-
-
-class DatasetSentencesIterator(object):
-    """ An iterator on sentences of the dataset. """
-
-    def __init__(self, x, y, phn_to_st, nframes=1):
-        self._x = x
-        self._y = y
-        self._start_end = [[0]]
-        self._nframes = nframes
-        self._memoized_x = defaultdict(lambda: {})
-        i = 0
-        for i, s in enumerate(y[0] == phn_to_st['!ENTER[2]']):
-            if s and i - self._start_end[-1][0] > MIN_FRAMES_PER_SENTENCE:
-                self._start_end[-1].append(i)
-                self._start_end.append([i])
-#            elif s:
-#                print "less than", MIN_FRAMES_PER_SENTENCE, "frames in",
-#                print self._start_end[-1][0], i
-        self._start_end[-1].append(i+1)
-
-    def _stackpad(self, start, end):
-        """ Method because of the memoization. """
-        if start in self._memoized_x and end in self._memoized_x[start]:
-            return self._memoized_x[start][end]
-        x = self._x[start:end]
-        nf = self._nframes
-        ret = numpy.zeros((x.shape[0], x.shape[1] * nf), dtype='float32')
-        ba = (nf - 1) / 2  # before/after
-        for i in xrange(x.shape[0]):
-            ret[i] = numpy.pad(x[max(0, i - ba):i + ba +1].flatten(),
-                    (max(0, (ba -i) * x.shape[1]),
-                        max(0, ((i + ba + 1) - x.shape[0]) * x.shape[1])),
-                    'constant', constant_values=(0, 0))
-        self._memoized_x[start][end] = ret
-        return ret
-
-    def __iter__(self):
-        """ Returns X, y_phn, y_spkr """
-        for start, end in self._start_end:
-            #yield shared(self._x[start:end], borrow=BORROW), shared(self._y[start:end], borrow=BORROW)
-            if self._nframes > 1:
-                #yield shared(self._stackpad(start, end)), shared(self._y[start:end])
-                yield self._stackpad(start, end), self._y[0][start:end], self._y[1][start:end]
-            else:
-                yield self._x[start:end], self._y[0][start:end], self._y[1][start:end]
 
 
 
