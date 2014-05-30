f7872e82db7dcbfbdf4fe09e49d2e08b6fc0ff39
syhw <gabriel.synnaeve@gmail.com>
2014-05-30 16:20:14 +0200

    run_exp AB neural nets params for debug

---
+++ DBN/launch_ab.sh
@@ -0,0 +1,3 @@
+python run_exp.py --dataset=/Users/gabrielsynnaeve/postdoc/datasets/TIMIT_train_dev_test/dtw_words_dev.joblib --nframes=7 --dataset-name="timit_dtw" --iterator-type=dtw --network-type=ab_net --debug-print --debug-time --debug-plot=1 --batch-size=200
+# --trainer-type=SGD --init-lr=0.00001
+
