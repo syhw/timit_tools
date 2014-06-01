e4d3144aecae3678e9817eb2c2d06f48740d4da1
syhw <gabriel.synnaeve@gmail.com>
2014-06-01 14:33:35 +0200

    fix stupid memory leak bug

--- DBN/dataset_iterators.py
+++ DBN/dataset_iterators.py
@@ -100,22 +100,30 @@ class DatasetDTWIterator(object):
         self._nwords (number of words per mini-batch).
         """
         ii = i/self._nwords
-        if i < len(self._x1_mem) and i < len(self._x2_mem):
+        if ii < len(self._x1_mem) and ii < len(self._x2_mem):
             return [[self._x1_mem[ii], self._x2_mem[ii]], self._y_mem[ii]]
 
+        nf = self._nframes
         def local_pad(x):
-            nf = self._nframes
             if nf <= 1:
                 return x
             if self._margin:
                 ma = self._margin
+                ba = (nf - 1) / 2  # before/after
                 ret = numpy.zeros((x.shape[0] - 2 * ma, x.shape[1] * nf),
                         dtype=theano.config.floatX)
-                for j in xrange(ret.shape[0]):
-                    ret[j] = x[j:j + 2*ma + 1].flatten()
+                if ba <= ma:
+                    for j in xrange(ret.shape[0]):
+                        ret[j] = x[j:j + 2*ma + 1].flatten()
+                else:
+                    for j in xrange(ret.shape[0]):
+                        ret[j] = numpy.pad(x[max(0, j - ba):j + ba +1].flatten(),
+                                (max(0, (ba - j) * x.shape[1]),
+                                    max(0, ((j + ba + 1) - x.shape[0]) * x.shape[1])),
+                                'constant', constant_values=(0, 0))
                 return ret
             else:
-                ret = numpy.zeros((x.shape[0] - 2*self._margin, x.shape[1] * nf),
+                ret = numpy.zeros((x.shape[0], x.shape[1] * nf),
                         dtype=theano.config.floatX)
                 ba = (nf - 1) / 2  # before/after
                 for j in xrange(x.shape[0]):
@@ -124,13 +132,27 @@ class DatasetDTWIterator(object):
                                 max(0, ((j + ba + 1) - x.shape[0]) * x.shape[1])),
                             'constant', constant_values=(0, 0))
                 return ret
+        
+        def cut_y(y):
+            ma = self._margin
+            if nf <= 1 or ma == 0:
+                return numpy.asarray(y, dtype='int8')
+            ret = numpy.zeros((y.shape[0] - 2 * ma), dtype='int8')
+            for j in xrange(ret.shape[0]):
+                ret[j] = y[j+ma]
+            return ret
 
-        self._x1_mem.append(numpy.concatenate([local_pad(self._x1[i+k]) for k
-            in xrange(self._nwords) if i+k < len(self._x1)]))
-        self._x2_mem.append(numpy.concatenate([local_pad(self._x2[i+k]) for k
-            in xrange(self._nwords) if i+k < len(self._x2)]))
-        self._y_mem.append(numpy.concatenate([self._y[i+k] for k in
-            xrange(self._nwords) if i+k < len(self._y)]))
+        x1_padded = [local_pad(self._x1[i+k]) for k 
+                in xrange(self._nwords) if i+k < len(self._x1)]
+        x2_padded = [local_pad(self._x2[i+k]) for k
+                in xrange(self._nwords) if i+k < len(self._x2)]
+        assert x1_padded[0].shape[0] == x2_padded[0].shape[0]
+        y_padded = [cut_y(self._y[i+k]) for k in
+            xrange(self._nwords) if i+k < len(self._y)]
+        assert x1_padded[0].shape[0] == len(y_padded[0])
+        self._x1_mem.append(numpy.concatenate(x1_padded))
+        self._x2_mem.append(numpy.concatenate(x2_padded))
+        self._y_mem.append(numpy.concatenate(y_padded))
         ii = i/self._nwords
         return [[self._x1_mem[ii], self._x2_mem[ii]], self._y_mem[ii]]
 
